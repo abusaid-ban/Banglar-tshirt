814686284ec59d250871a5ac4924273fe12d397a
unknown <personal.abusaid@gmail.com>
2023-04-07 10:28:31 +0600

    creating banglar tshirt by react router

---
+++ src/main.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './Components/Home/Home';
+import Main from './Components/Layout/main';
+import Orders from './Components/OrderReviews/Orders';
+
+const router= createBrowserRouter([
+  {
+    path:'/',
+    element:<Main></Main>,
+    children:[
+      {
+        path:'/',
+        element:<Home></Home>,
+        loader:()=>fetch('tshirts.json')
+      },
+      {
+        path:'/orders',
+        element:<Orders></Orders>
+      },
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+      <RouterProvider router={router} />
+  </React.StrictMode>,
+)
