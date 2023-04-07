50968dc51087635afdd6d4afb25173ca09e5abf9
unknown <personal.abusaid@gmail.com>
2023-04-07 10:58:08 +0600

    make tshirt layout and added ton cart

---
+++ src/Components/Tshirt/Tshirt.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Tshirt.css';
+const Tshirt = ({tshirt, handleAddToCart}) => {
+    const {picture,name,price}= tshirt;
+    return (
+        <div className='tshirt'>
+            <img src={picture} alt="" />
+            <h4>Product Name:{name}</h4>
+            <p>Price: ${price}</p>
+            <button onClick={handleAddToCart}>Buy Now</button>
+        </div>
+    );
+};
+
+export default Tshirt;
